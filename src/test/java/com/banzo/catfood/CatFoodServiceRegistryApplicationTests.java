0994b0a5b292eed5125d3a5d04e42032f1fbe72b
banzo276 <matdudek2706@gmail.com>
2022-01-01 20:54:45 +0100

    Initial commit

---
+++ src/test/java/com/banzo/catfood/CatFoodServiceRegistryApplicationTests.java
@@ -0,0 +1,11 @@
+package com.banzo.catfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatFoodServiceRegistryApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
